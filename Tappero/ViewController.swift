1c3c43caec7650dc62b53c532f0997365b60e42b
Stevhen <stevhen@Stevhens-MacBook-Pro.local>
2020-03-12 08:24:46 +0700

    Change button with picker

--- Tappero/ViewController.swift
+++ Tappero/ViewController.swift
@@ -8,40 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    @IBOutlet weak var logoBg: UIView!
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapCountLabel: UILabel!
     @IBOutlet weak var tapGoalTextField: UITextField!
     @IBOutlet weak var playBg: UIView!
     @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var gameOverLabel: UILabel!
     
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var thirtySecondsBtn: UIButton!
-    @IBOutlet weak var fourtyFiveSecondsBtn: UIButton!
-    @IBOutlet weak var sixtySecondsBtn: UIButton!
     
     var maxTaps : Int = 0
     var currTaps : Int = 0
     var seconds : Int = 0
     var timer = Timer()
     var isTimerRunning = false
+    var selectedSeconds : String? = ""
+    let timeSeconds = ["30","45","60"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tapCountLabel.isHidden = true
         tapBtn.isHidden = true
+        
         tapGoalTextField.keyboardType = .asciiCapableNumberPad
         tapCountLabel.textColor = UIColor(red:0.31, green:0.32, blue:0.66, alpha:1.0)
         timerLabel.textColor = UIColor(red:0.31, green:0.32, blue:0.66, alpha:1.0)
-        self.logoBg.layer.cornerRadius = 10
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
         self.playBg.layer.cornerRadius = 20
-        self.thirtySecondsBtn.layer.cornerRadius = 10
-        self.fourtyFiveSecondsBtn.layer.cornerRadius = 10
-        self.sixtySecondsBtn.layer.cornerRadius = 10
+        tapGoalTextField.layer.cornerRadius = 20
 //        self.playBg.layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
 //        self.playBtn.layer.shadowOpacity = 0.4
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
@@ -61,6 +64,27 @@ class ViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timeSeconds.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return timeSeconds[row] + " Seconds"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedSeconds = timeSeconds[row]
+        if let a = selectedSeconds {
+            seconds = (a as NSString).integerValue
+        } else {
+            seconds = 30
+        }
+    }
+    
     override func dismissKeyboard(){
         tapGoalTextField.endEditing(true)
     }
@@ -78,12 +102,8 @@ class ViewController: UIViewController {
             self.view.frame.origin.y = 0
         }
     }
-
     
     fileprivate func hideSecondsBtn() {
-        thirtySecondsBtn.isHidden = true
-        fourtyFiveSecondsBtn.isHidden = true
-        sixtySecondsBtn.isHidden = true
     }
     
     @IBAction func playBtnPressed(_ sender: Any) {
@@ -94,8 +114,14 @@ class ViewController: UIViewController {
             hideSecondsBtn()
             timerLabel.isHidden = false
             
+            if seconds == 0 {
+                seconds = (timeSeconds[0] as NSString).integerValue
+            }
+            timerLabel.text = "\(seconds)"
+            
             maxTaps = Int(tapGoalTextField.text!)!
             showTapImage()
+            pickerView.isHidden = true
             
             if isTimerRunning == false {
                 isTimerRunning = true
@@ -120,7 +146,7 @@ class ViewController: UIViewController {
             restartGame()
         }
         
-        timerLabel.text = "Time left: \(seconds)"
+        timerLabel.text = "\(seconds)"
     }
     
     func showFailAlert() {
@@ -147,13 +173,63 @@ class ViewController: UIViewController {
         updateTapCountLabel()
         
         if isEnded() {
-            showWinAlert()
-            restartGame()
+//            showWinAlert()
+//            let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//            myLabel.text = "GameOver!"
+//            myLabel.fontSize = 65
+//            myLabel.position = CGPoint(x: viewSize.width * 0.5, y: viewSize.height * 0.65)
+//            self.addChild(myLabel)
+            
+            gameOver()
+            timer.invalidate()
+            
+            //delay
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { // Change `2.0` to the desired number of seconds.
+               // Code you want to be delayed
+                self.restartGame()
+            }
         }
     }
     
+    func gameOver(){
+        //UIImage
+        UIView.transition(with: tapBtn,
+                          duration: 5.00,
+                          options: .curveEaseIn,
+        animations: {
+            if let image = UIImage(named: "gameOver") {
+                self.tapBtn.setImage(image, for: .normal)
+            }
+        },
+        completion: nil)
+        tapBtn.isEnabled = false
+        gameOverLabel.isHidden = false
+        
+        UIView.animate(withDuration: 1, delay: 0.2, options: [.transitionCrossDissolve], animations: {
+            self.gameOverLabel.text = "GAME OVER"
+            self.gameOverLabel.center.x = self.view.bounds.width - 230.0
+              self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        
+//        let winner = SKLabelNode(fontNamed: "Chalkduster")
+//        winner.text = "You Win!"
+//        winner.fontSize = 65
+//        winner.fontColor = SKColor.green
+//        winner.position = CGPoint(x: 82, y: 423)
+//
+//        self.addChild(winner)
+        
+        //game over animation
+//        gameOverLbl = SKLabelNode(fontNamed:"Chalkduster")
+//        gameOverLbl.text = "GameOver!"
+//        gameOverLbl.fontSize = 65
+//        gameOverLbl.position = CGPoint(x: 82, y: 423)
+//        self.addChild(gameOverLbl! as UIViewController)
+    }
+    
     func updateTapCountLabel() {
-        tapCountLabel.text = "\(currTaps) Taps"
+        tapCountLabel.text = "\(currTaps)"
     }
     
     func showWinAlert() {
@@ -199,35 +275,35 @@ class ViewController: UIViewController {
 //        thirtySecondsBtn.isHighlighted = false
 //        fourtyFiveSecondsBtn.isHighlighted = false
 //        sixtySecondsBtn.isHighlighted = false
-        thirtySecondsBtn.setImage(UIImage(named: "30.png"), for: .normal)
-        fourtyFiveSecondsBtn.setImage(UIImage(named: "45.png"), for: .normal)
-        sixtySecondsBtn.setImage(UIImage(named: "60.png"), for: .normal)
+//        thirtySecondsBtn.setImage(UIImage(named: "30.png"), for: .normal)
+//        fourtyFiveSecondsBtn.setImage(UIImage(named: "45.png"), for: .normal)
+//        sixtySecondsBtn.setImage(UIImage(named: "60.png"), for: .normal)
     }
     
-    @IBAction func timerBtnPressed(_ sender: UIButton) {
-        resetBtnColor()
-        
-        if sender == thirtySecondsBtn {
-//            thirtySecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
-            thirtySecondsBtn.setImage(UIImage(named: "30dark"), for: .normal)
-
-            seconds = 30
-        }
-        else if sender == fourtyFiveSecondsBtn {
-//            fourtyFiveSecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
-            fourtyFiveSecondsBtn.setImage(UIImage(named: "45dark"), for: .normal)
-            seconds = 45
-        }
-        else if sender == sixtySecondsBtn {
-//            sixtySecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
-//            sixtySecondsBtn.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
-            sixtySecondsBtn.setImage(UIImage(named: "60dark"), for: .normal)
-            seconds = 60
-        }
-        
-        timerLabel.text = "Time left: \(seconds)"
-        
-    }
+//    @IBAction func timerBtnPressed(_ sender: UIButton) {
+//        resetBtnColor()
+//
+//        if sender == thirtySecondsBtn {
+////            thirtySecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+//            thirtySecondsBtn.setImage(UIImage(named: "30dark"), for: .normal)
+//
+//            seconds = 30
+//        }
+//        else if sender == fourtyFiveSecondsBtn {
+////            fourtyFiveSecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+//            fourtyFiveSecondsBtn.setImage(UIImage(named: "45dark"), for: .normal)
+//            seconds = 45
+//        }
+//        else if sender == sixtySecondsBtn {
+////            sixtySecondsBtn.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+////            sixtySecondsBtn.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+//            sixtySecondsBtn.setImage(UIImage(named: "60dark"), for: .normal)
+//            seconds = 60
+//        }
+//
+//        timerLabel.text = "Time left: \(seconds)"
+//
+//    }
     
     
     func showTapImage() {
@@ -237,33 +313,41 @@ class ViewController: UIViewController {
         logoImg.isHidden = true
         playBtn.isHidden = true
         tapGoalTextField.isHidden = true
-        logoBg.isHidden = true
+//        logoBg.isHidden = true
         playBg.isHidden = true
     }
     
     func restartGame(){
         maxTaps = 0
         currTaps = 0
-        resetBtnColor()
+//        resetBtnColor()
+        pickerView.selectRow(0, inComponent: 0, animated: true)
 
         playBtn.isHidden = false
         tapGoalTextField.text = ""
         tapGoalTextField.isHidden = false
-        logoBg.isHidden = false
+//        logoBg.isHidden = false
         logoImg.isHidden = false
         playBg.isHidden = false
         
         tapBtn.isHidden = true
         tapCountLabel.text = ""
         tapCountLabel.isHidden = true
-        thirtySecondsBtn.isHidden = false
-        fourtyFiveSecondsBtn.isHidden = false
-        sixtySecondsBtn.isHidden = false
+        pickerView.isHidden = false
+//        thirtySecondsBtn.isHidden = false
+//        fourtyFiveSecondsBtn.isHidden = false
+//        sixtySecondsBtn.isHidden = false
         
         timerLabel.isHidden = true
         isTimerRunning = false
+        gameOverLabel.isHidden = true
+        tapBtn.isEnabled = true
         
         timer.invalidate()
+        
+        if let image = UIImage(named: "star3") {
+            self.tapBtn.setImage(image, for: .normal)
+        }
     }
     
     func isEnded() -> Bool {
